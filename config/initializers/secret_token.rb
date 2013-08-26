ef5096eac4c7a231117e694bc47bce60c6c96fb5
TurtleShip <sk765@cornell.edu>
2013-08-26 15:12:28 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cs2048Homepage::Application.config.secret_token = 'da51a74256385d217022e14526d98f3c09257aabe8d4fc8b414838eb10aa08f044509149d34683b369c4c7ba5267839fe35cf0f0409acf6193402cf8d6015aa7'
