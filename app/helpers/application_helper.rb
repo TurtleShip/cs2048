454c48c2d7ffa88c363b5a146b9d7d127988e14d
TurtleShip <sk765@cornell.edu>
2013-08-26 23:53:02 -0400

    Added titles to each page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    base_title = "CS2048"
+
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
 end
